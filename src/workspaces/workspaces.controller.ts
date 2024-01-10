ad226608b413a5426d47f0b6ea280f8895bfbffd
=lemon-table <=>
2024-01-10 18:14:42 +0900

    fix: controller UseGuards 변경

--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -2,9 +2,10 @@ import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from 
 import { WorkspacesService } from './workspaces.service';
 import { CreateWorkspaceDto } from './dto/create-workspace.dto';
 import { MembersDto } from './dto/invite-member.dto';
-import { BearerTokenGuard } from 'src/auth/guard/bearer.guard';
+import { AccessTokenGuard } from 'src/auth/guard/bearer.guard';
 
 @Controller('workspaces')
+@UseGuards(AccessTokenGuard)
 export class WorkspacesController {
   constructor(private readonly workspacesService: WorkspacesService) {}
 
@@ -13,7 +14,6 @@ export class WorkspacesController {
      * (로그인 한 사용자 가능)
      */
   @Post()
-  @UseGuards(BearerTokenGuard)
   async createWorkspace(@Req() req: Request,@Body() createworkspaceDto: CreateWorkspaceDto) {
     return await this.workspacesService.createWorkspace(req['userId'],createworkspaceDto);
   }
@@ -23,7 +23,6 @@ export class WorkspacesController {
      * (멤버 추가된 워크스페이스만 조회)
      */
   @Get()
-  @UseGuards(BearerTokenGuard)
   async findAllWorkspace(@Req() req: Request) {
     return await this.workspacesService.findAllWorkspace(req['userId']);
   }
@@ -33,7 +32,6 @@ export class WorkspacesController {
      * (워크스페이스 운영자만 가능)
      */
   @Put(':workspaceId')
-  @UseGuards(BearerTokenGuard)
     async updateWorkspace(@Req() req: Request,@Param('workspaceId') workspaceId: number,@Body() createworkspaceDto: CreateWorkspaceDto) {
       return await this.workspacesService.updateWorkspace(req['userId'],workspaceId,createworkspaceDto);
   }
@@ -43,7 +41,6 @@ export class WorkspacesController {
      * (워크스페이스 운영자만 가능)
      */
   @Delete(':workspaceId')
-  @UseGuards(BearerTokenGuard)
   async deleteWorkspace(@Req() req: Request, @Param('workspaceId') workspaceId: number) {
     return await this.workspacesService.deleteWorkspace(req['userId'],workspaceId);
   }
@@ -53,7 +50,6 @@ export class WorkspacesController {
      * (워크스페이스 운영자만 가능)
      */
   @Post(':workspaceId/invite')
-  @UseGuards(BearerTokenGuard)
   async inviteMember(@Req() req: Request, @Body() membersDto: MembersDto, @Param('workspaceId') workspaceId: number) {
     return await this.workspacesService.inviteMembers(req['userId'],workspaceId,membersDto);
   }
