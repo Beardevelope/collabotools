00343bd3f7f7daa5a65c6a57c0706aea235c993d
hhsukyu <hhsukyu@gmail.com>
2024-01-10 14:20:48 +0900

    feat:list의 auth기능 추가 작성 test 완료

--- src/lists/lists.controller.ts
+++ src/lists/lists.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Post, Get, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Param, Body, UseGuards } from '@nestjs/common';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
 import { OrderListDto } from './dto/order-list.dto';
 import { ListsService } from './lists.service';
+import { BearerTokenGuard } from 'src/auth/guard/bearer.guard';
 
 @Controller('list')
 export class ListsController {
     constructor(private readonly listsService: ListsService) {}
 
     @Post(':workspaceId')
+    @UseGuards(BearerTokenGuard)
     async createList(
         @Param('workspaceId') workspaceId: number,
         @Body() createListDto: CreateListDto,
@@ -22,6 +24,7 @@ export class ListsController {
     }
 
     @Put(':workspaceId/:id')
+    @UseGuards(BearerTokenGuard)
     async updateList(
         @Param('workspaceId') workspaceId: number,
         @Param('id') id: number,
@@ -31,11 +34,13 @@ export class ListsController {
     }
 
     @Delete(':workspaceId/:id')
+    @UseGuards(BearerTokenGuard)
     deleteList(@Param('workspaceId') workspaceId: number, @Param('id') id: number) {
         return this.listsService.deleteList(workspaceId, id);
     }
 
     @Put(':workspaceId/:id/order')
+    @UseGuards(BearerTokenGuard)
     updateListOrder(
         @Param('workspaceId') workspaceId: number,
         @Param('id') id: number,
--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ListsModel } from './entities/lists.entity';
-import { WorkspacesModel } from 'src/workspaces/entities/workspaces.entity';
 import { ListsController } from './lists.controller';
 import { ListsService } from './lists.service';
+import { WorkspacesModel } from 'src/workspaces/entities/workspaces.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([ListsModel, WorkspacesModel])],
+    imports: [TypeOrmModule.forFeature([ListsModel, WorkspacesModel]), AuthModule],
     controllers: [ListsController],
     providers: [ListsService],
 })
